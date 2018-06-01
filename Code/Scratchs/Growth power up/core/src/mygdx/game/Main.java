b45c25788e67da9108f6d91ef40314543ced725b
Unknown <mattttpatttt@gmail.com>
2018-06-01 10:05:07 -0400

    Growth power up

--- Code/Scratchs/Growth power up/core/src/mygdx/game/Main.java
+++ Code/Scratchs/Growth power up/core/src/mygdx/game/Main.java
@@ -2,32 +2,40 @@ package mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
-	@Override
+     Texture img1;
+    Sprite sprGrowth;
+
+    @Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("growth pwoer up.png");
-	}
+		img1 = new Texture("growth pwoer up.png");
+        sprGrowth = new Sprite(img1);
+        sprGrowth.setPosition(0,0);
+        sprGrowth.setScale(1f);
+    }
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+        Gdx.gl.glClearColor(0, 0, 0, 0);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        sprGrowth.draw(batch);
+        batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		img1.dispose();
 	}
 }
