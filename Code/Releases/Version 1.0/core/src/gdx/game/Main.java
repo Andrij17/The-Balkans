14865dc09b71d40eab624aeea42c75c922a4c1ce
Unknown <andrijaisakov@gmail.com>
2018-06-07 11:06:37 -0400

    Version 1.1
    Mario jumping, goomba sliding

--- Code/Releases/Version 1.0/core/src/gdx/game/Main.java
+++ Code/Releases/Version 1.0/core/src/gdx/game/Main.java
@@ -2,6 +2,7 @@ package gdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -10,16 +11,37 @@ public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	SprMario sprMario;
     SprGoomba sprGoomba;
+    double dYspeed;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		sprMario= new SprMario(50,50,50,50);
-        sprGoomba=new SprGoomba(50,50,100,100);
-	}
+        sprMario= new SprMario(90,160,50,50);
+        sprGoomba=new SprGoomba(690,700,600,50);
+    }
 
 	@Override
 	public void render () {
+        //Mario keyboard control
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
+            sprMario.setX(sprMario.getX() -4);
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+            sprMario.setX(sprMario.getX() +4);
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+            dYspeed=10;
+            sprMario.setY(sprMario.getY() +9);
+        }
+        if(sprMario.getY()>50){
+            dYspeed-=1;
+            sprMario.translateY((float) dYspeed);
+        }
+        if(sprMario.getY()>400){
+            sprMario.setY(400);
+        }
+
+        //Goomba sliding
+        sprGoomba.setX(sprGoomba.getX() -1);
+
 		Gdx.gl.glClearColor(0, 0.498f, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
